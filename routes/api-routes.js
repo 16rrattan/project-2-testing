26471412b531a4045ccf5913154ad23b377e10f1
Kevin Spies <kevin.spies@gmail.com>
2019-08-04 05:19:02 -0400

    grabbing one author and loading in that authors haiku's - still too conceptual 'going in circles'

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -3,6 +3,24 @@ var db = require("../models");
 
 module.exports = function (app) {
 
+//loads someones edit page
+  app.get("/api/authors/:id", function (req, res) {
+    // Here we add an "include" property to our options in our findOne query
+    // We set the value to an array of the models we want to include in a left outer join
+    // In this case, just db.Post
+    db.Author.findOne({
+      where: {
+        id: req.params.id
+      },
+      include: [db.Post]
+    }).then(function (dbAuthor) {
+      res.json(dbAuthor);
+    });
+  });
+
+
+
+
     //post route to make a new author
     app.post("api/authors", function (req, res) {
         db.Author.create(req.body).then(function (dbAuthor) {
